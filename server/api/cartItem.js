6bfa74cbbdd60edf7eb7f481ae51d04169b0c185
Alex Sun <alexsun424@gmail.com>
2022-11-09 12:04:11 -0500

    made revisions + added try/catches to everything

--- server/api/cartItem.js
+++ server/api/cartItem.js
@@ -1,41 +1,64 @@
 const router = require("express").Router()
 const {models : {CartItem, Product}} = require("../db")
 
-router.get("/allCartItems/:userId", async (req, res) => {
-    const allCartItems = CartItem.findAll({
-        include: {model : Product},
-        where: {userId: req.params.userId}
-    })
-    res.send(allCartItems)
+router.get("/allCartItems/:userId", async (req, res, next) => {
+    try{
+        const allCartItems = CartItem.findAll({
+            include: {model : Product},
+            where: {userId: req.params.userId}
+        })
+        res.send(allCartItems)
+    } catch (err){
+        next(err)
+    }
 })
 
-router.put("/changeCartQuantity/:userId/:productId", async (req, res) => {
-    const updatedQuantity = req.body
-    const findCartItem = CartItem.findOne({
-        where: {userId: req.params.userId, productId: req.params.productId}
-    })
-    await findCartItem.update({quantity: updatedQuantity})
-    res.send(findCartItem)
+router.put("/changeCartQuantity/:userId/:productId", async (req, res, next) => {
+    try{
+        const updatedQuantity = req.body
+        const findCartItem = CartItem.findOne({
+            where: {userId: req.params.userId, productId: req.params.productId}
+        })
+        await findCartItem.update({quantity: updatedQuantity})
+        res.send(findCartItem)
+    } catch (err){
+        next(err)
+    }
 })
 
-router.post("/addCartItem", async (req, res) => {
-    const newCartItem = req.body
-    const newItem = CartItem.create(newCartItem)
-    res.send(newItem)
+router.post("/addCartItem", async (req, res, next) => {
+    try{
+        const newCartItem = req.body
+        const newItem = CartItem.create(newCartItem)
+        res.send(newItem)
+    } catch (err){
+        next(err)
+    }
 })
 
-router.delete("/deleteCartItem/:userId/:productId", async (req, res) => {
-    const itemToDelete = CartItem.findOne({
-        where: {userId: req.params.userId, productId: req.params.productId}
-    })
-    await CartItem.destroy({
-        where: {userId: req.params.userId, productId: req.params.productId}
-    })
-   res.send(itemToDelete)
+router.delete("/deleteSingleCartItem/:userId/:productId", async (req, res, next) => {
+    try{
+        const itemToDelete = CartItem.findOne({
+            where: {userId: req.params.userId, productId: req.params.productId}
+        })
+        await CartItem.destroy({
+            where: {userId: req.params.userId, productId: req.params.productId}
+        })
+       res.send(itemToDelete)
+    } catch(err){
+        next(err)
+    }
 })
 
-
-router.delete
-
+router.delete("/deleteAllCartItems/:userId", async (req, res, next) => {
+    try{
+        await CartItem.destroy({
+            where: {userId: req.params.userId}
+        })
+        res.send(null)
+    } catch(err){
+        next(err)
+    }
+})
 
 module.exports = router
