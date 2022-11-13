42b059cea65c0010c16d81b92923433a746dc7ff
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-11-13 18:25:14 -0500

    initial cart page - more updates to come

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,4 +1,78 @@
-import React, {useEffect} from "react"
-import {useSelector, useDispatch} from "react-redux"
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchCartUser,
+  addItemUser,
+  updateQuantityUser,
+  deleteItemUser,
+  clearCartUser,
+  setCartLocal,
+  addItemLocal,
+  deleteItemLocal,
+  updateQuantityLocal,
+  clearCartLocal,
+  calcTotals,
+} from "../store/cart";
 
+const Cart = () => {
+  const { cartItems, quantityTotal, priceTotal, isLoading } = useSelector(
+    (store) => store.cart
+  );
+  const dispatch = useDispatch();
 
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (quantityTotal === 0) {
+    return <h1>Cart is Empty</h1>;
+  }
+
+  return (
+    <div className="cartDisplay">
+      {cartItems.map((cartItem) => {
+        return (
+          <div className="cartItem" key={cartItem.id}>
+            <img id="cartItem-img" src={cartItem.product.imageURL} />
+            <div className="cartItem-text">
+              <h4 id="cartItem-title">{cartItem.product.title}</h4>
+              <p id="cartItem-artist">{cartItem.product.artist}</p>
+              <p id="cartItem-price">Price: ${cartItem.product.displayPrice}</p>
+              <p id="cartItem-quantity">Quantity: {cartItem.quantity}</p>
+              <p id="cartItem-totalPrice">
+                Total: ${cartItem.quantity * cartItem.product.displayPrice}
+              </p>
+              <button
+                onClick={() => {
+                  dispatch(deleteItemUser(cartItem.id));
+                }}
+              >
+                Remove Item
+              </button>
+              <div>
+                <form
+                  onSubmit={(evt) => {
+                    evt.preventDefault();
+                    console.log(evt.target)
+                    //dispatch(updateQuantityUser(evt.target.value));   /need to set up modularized cart items with local state for forms
+                  }}
+                >
+                  <label htmlFor="quantity">Update Quantity:</label>
+                  <input
+                    type="number"
+                    name="quantity"
+                    step="1"
+                    defaultValue={cartItem.quantity}
+                  />
+                  <input type="submit" />
+                </form>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cart;
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,24 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+const TOKEN = "token";
 
 //need different paths for users and guests
-//need some way to check if an added item is already in cart and update quantity instead (possibly in component?)
 
-// Async Think
-export const fetchUserCart = createAsyncThunk(
+////// Async Thunks
+
+// get all items for logged in users
+export const fetchCartUser = createAsyncThunk(
   "/cart/fetchCartItems",
   async () => {
     try {
-      let response;
-      if (productInfo) {
-        const { limit, offset, filterCategory, filter, order, scale } =
-          productInfo;
-        response = await axios.get(
-          `/api/products?limit=${limit}&offset=${offset}&filterCategory=${filterCategory}&filter=${filter}&order=${order}&scale=${scale}`
-        );
-      } else {
-        response = await axios.get("/api/products");
-      }
+      const token = window.localStorage.getItem(TOKEN);
+      const response = await axios.get("/api/cartItems", {
+        headers: {
+          authorization: token,
+        },
+      });
       return response.data;
     } catch (err) {
       console.log(err);
@@ -26,7 +24,6 @@ export const fetchUserCart = createAsyncThunk(
   },
   {
     condition: () => {
-      // can take parameters from async f(X) above and {getState, extra} as last parameter
       const token = window.localStorage.getItem("token");
       if (token === null) {
         return false;
@@ -35,11 +32,123 @@ export const fetchUserCart = createAsyncThunk(
   }
 );
 
+// add item for logged in user
+export const addItemUser = createAsyncThunk(
+  "/cart/addItemUser",
+  async ({ productId, quantity }, { dispatch }) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      const item = await axios.post("/api/cartItems", {
+        headers: {
+          authorization: token,
+        },
+        body: {
+          productId,
+          quantity,
+        },
+      });
+      dispatch(fetchCartUser());
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  {
+    condition: () => {
+      const token = window.localStorage.getItem("token");
+      if (token === null) {
+        return false;
+      }
+    },
+  }
+);
+
+// update item quantity for logged in user
+export const updateQuantityUser = createAsyncThunk(
+  "/cart/updateQuantityUser",
+  async ({ cartId, quantity }, { dispatch }) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      const item = await axios.put(`/api/cartItems/${cartId}`, {
+        headers: {
+          authorization: token,
+        },
+        body: {
+          quantity,
+        },
+      });
+      dispatch(fetchCartUser());
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  {
+    condition: () => {
+      const token = window.localStorage.getItem("token");
+      if (token === null) {
+        return false;
+      }
+    },
+  }
+);
+
+// delete item for logged in user
+export const deleteItemUser = createAsyncThunk(
+  "/cart/deleteItemUser",
+  async (cartId, { dispatch }) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      await axios.delete(`/api/cartItems/${cartId}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(fetchCartUser());
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  {
+    condition: () => {
+      const token = window.localStorage.getItem("token");
+      if (token === null) {
+        return false;
+      }
+    },
+  }
+);
+
+// clear cart for logged in user
+export const clearCartUser = createAsyncThunk(
+  "/cart/clearCartUser",
+  async ({ dispatch }) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      await axios.delete(`/api/cartItems`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(fetchCartUser());
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  {
+    condition: () => {
+      const token = window.localStorage.getItem("token");
+      if (token === null) {
+        return false;
+      }
+    },
+  }
+);
+
+// Initial State
 const initialState = {
   cartItems: [],
   quantityTotal: 0,
   priceTotal: 0,
-  isLoading: true,
+  isLoading: false,
 };
 
 // Create Slice
@@ -47,39 +156,88 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem: (state, action) => {
-      state.cartItems = state.cartItems.push(action.payload);
+    setCartLocal: (state, action) => {
+      const cart = window.localStorage.getItem("cart");
+      if (cart) state.cartItems = cart;
     },
-    deleteItem: (state, action) => {
+    saveCartLocal: (state, action) => {
+      window.localStorage.setItem("cartItems", state.cartItems);
+    },
+    addItemGuest: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.cartItems.find(
+        (item) => item.id === newItem.id
+      );
+      if (existingItem) {
+        existingItem.quantity += newItem.quantity;
+      } else {
+        state.cartItems.push(action.payload);
+      }
+    },
+    deleteItemGuest: (state, action) => {
       const itemId = action.payload;
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
-    updateQuantity: (state, action) => {
+    updateQuantityGuest: (state, action) => {
       const { id: itemId, quantity } = action.payload;
       const cartItem = state.cartItems.find((item) => item.id === itemId);
       cartItem.quantity = quantity;
     },
-    clearCart: (state, action) => {
+    clearCartGuest: (state, action) => {
       return initialState;
     },
     calcTotals: (state, action) => {
-      let quantityTotal = (priceTotal = 0);
+      let quantityTotal = 0;
+      let priceTotal = 0;
       state.cartItems.forEach((item) => {
         quantityTotal += item.quantity;
-        priceTotal += item.price * item.quantity;
+        priceTotal += item.product.displayPrice * item.quantity;
       });
       state.quantityTotal = quantityTotal;
       state.priceTotal = priceTotal;
     },
   },
-  // //   extraReducers: (builder) => {
-  // //     builder
-  // //       .addCase(fetchProducts.pending, (state, action) => state)
-  // //       .addCase(fetchProducts.fulfilled, (state, action) => action.payload)
-  // //       .addCase(fetchProducts.rejected, (state, action) => state);
-  //   }, // you can add more .addCase as you need
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCartUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchCartUser.fulfilled, (state, action) => {
+        state.cartItems = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchCartUser.rejected, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(addItemUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(updateQuantityUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteItemUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(clearCartUser.pending, (state, action) => {
+        state.isLoading = true;
+      });
+  },
 });
 
 export default cartSlice.reducer;
-const addItem = cartSlice.actions.addItem;
-export { addItem };
+const setCartLocal = cartSlice.actions.setCartLocal;
+const saveCartLocal = cartSlice.actions.saveCartLocal;
+const addItemGuest = cartSlice.actions.addItemGuest;
+const deleteItemGuest = cartSlice.actions.deleteItemGuest;
+const updateQuantityGuest = cartSlice.actions.updateQuantityGuest;
+const clearCartGuest = cartSlice.actions.clearCartGuest;
+const calcTotals = cartSlice.actions.calcTotals;
+export {
+  setCartLocal,
+  saveCartLocal,
+  addItemGuest,
+  deleteItemGuest,
+  updateQuantityGuest,
+  clearCartGuest,
+  calcTotals,
+};
